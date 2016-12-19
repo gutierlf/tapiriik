5b58d54cafb583d793be1e5f743e1e3fbb0704e7
Luis F Gutierrez <luis@gutierrezhiller.com>
2016-12-19 14:25:10 -0800

    remove obsolete tests used for refactoring DownloadActivity method
    Unit-level tests now exist for Strava#connection and
    Strava#activity_gateway. StravaServie#DownloadActivity is now
    tested just by ensuring the interactions with those other classes
    via mocks.

--- tapiriik/testing/services/Strava/test_download_activity.py
+++ tapiriik/testing/services/Strava/test_download_activity.py
@@ -1,46 +1,21 @@
-from datetime import timedelta
+from unittest import mock
 
 import pytz
 
 from tapiriik.testing.testtools import TapiriikTestCase, TestTools
-from tapiriik.testing.services.Strava.connection_stubs import Http401Getter, HttpNoJsonGetter, HttpRecordNotFoundGetter, \
-    HttpErrorInDownloadedDataGetter, FileLoader
+from tapiriik.testing.services.Strava.connection_stubs import FileLoader
 from tapiriik.services.Strava import StravaService
-from tapiriik.services.api import APIException
-from tapiriik.services.interchange import Waypoint, WaypointType, Location, Lap
+from tapiriik.services.interchange import Lap
 
 TEST_ACTIVITY_ID = 692697310
 
 class StravaServiceDownloadActivityTests(TapiriikTestCase):
     def setUp(self):
-        svc = TestTools.create_mock_service("Strava")
+        svc = TestTools.create_mock_service('Strava')
         self.svcRecord = TestTools.create_mock_svc_record(svc)
         self.svcRecord.Authorization = {"OAuthToken": "token"}
         self.activity = TestTools.create_blank_activity(record=self.svcRecord)
         self.activity.ServiceData = {"Manual": False, "ActivityID": 1}
-
-    def testStatusCode401RaisesAPIException(self):
-        self.assertDownloadActivityRaisesAPIException(
-            Http401Getter, "No authorization to download activity")
-
-    def testMissingJsonRaisesAPIException(self):
-        self.assertDownloadActivityRaisesAPIException(
-            HttpNoJsonGetter, "Stream data returned is not JSON")
-
-    def testRecordNotFoundMessageRaisesAPIException(self):
-        self.assertDownloadActivityRaisesAPIException(
-            HttpRecordNotFoundGetter, "Could not find activity")
-
-    def testErrorInDownloadedDataRaisesAPIException(self):
-        self.assertDownloadActivityRaisesAPIException(
-            HttpErrorInDownloadedDataGetter, "Strava error the error message")
-
-    def assertDownloadActivityRaisesAPIException(self, http_getter, message):
-        with self.assertRaises(APIException) as cm:
-            StravaService().DownloadActivity(self.svcRecord,
-                                             self.activity,
-                                             connection=http_getter)
-        self.assertEqual(cm.exception.Message, message)
 
     def testManualActivitySetsSimpleLap(self):
         self.activity.ServiceData["Manual"] = True
@@ -51,108 +26,29 @@ class StravaServiceDownloadActivityTests(TapiriikTestCase):
                            stats=self.activity.Stats)
         self.assertLapsEqual(self.activity.Laps[0], expected_lap)
 
+    @mock.patch('tapiriik.services.Strava.connection')
+    def testConnectionGetActivityIsCalled(self, mock_connection):
+        self.activity.ServiceData["ActivityID"] = TEST_ACTIVITY_ID
+        self.activity.StartTime = self.activity.StartTime.replace(tzinfo=pytz.utc)
+        streamdata = FileLoader.getActivity(TEST_ACTIVITY_ID, None)
+        mock_connection.getActivity.return_value = streamdata
+        StravaService().DownloadActivity(self.svcRecord, self.activity,
+                                         connection=mock_connection)
+        mock_connection.getActivity.assert_called()
+
+    @mock.patch('tapiriik.services.Strava.activity_gateway')
+    def testActivityGatewayGetWaypointsCalled(self, mock_gateway):
+        self.activity.ServiceData["ActivityID"] = TEST_ACTIVITY_ID
+        StravaService().DownloadActivity(self.svcRecord, self.activity,
+                                         connection=FileLoader)
+        mock_gateway.get_waypoints.assert_called()
+
     def testActivityDataStoredInSingleLap(self):
         self.assertEqual(len(self.activity.Laps), 0)
         self.activity.ServiceData["ActivityID"] = TEST_ACTIVITY_ID
-        StravaService().DownloadActivity(self.svcRecord, self.activity, connection=FileLoader)
+        StravaService().DownloadActivity(self.svcRecord, self.activity,
+                                         connection=FileLoader)
         self.assertEqual(len(self.activity.Laps), 1)
         self.assertEqual(self.activity.Laps[0].Stats, self.activity.Stats)
         self.assertEqual(self.activity.Laps[0].StartTime, self.activity.StartTime)
         self.assertEqual(self.activity.Laps[0].EndTime, self.activity.EndTime)
-
-    def testStreamProcessingFacts(self):
-        self._testStreamProcessingFacts(TEST_ACTIVITY_ID)
-
-    def testJustTimeAndMoving(self):
-        self._testStreamProcessingFacts('just_time_and_moving')
-
-    def testJustTimeAndMovingWithPauses(self):
-        self._testStreamProcessingFacts('just_time_and_moving.true_true_false')
-        self._testStreamProcessingFacts('just_time_and_moving.false_true_false')
-        self._testStreamProcessingFacts('just_time_and_moving.false_false_false')
-        self._testStreamProcessingFacts('just_time_and_moving.true_false_false')
-        self._testStreamProcessingFacts('just_time_and_moving.with_middle_pause')
-
-    def testLatLng(self):
-        self._testStreamProcessingFacts('latlng')
-
-    def testLatLngWithZeros(self):
-        self._testStreamProcessingFacts('latlng.with_zeros')
-
-    def testAltitude(self):
-        self._testStreamProcessingFacts('altitude')
-
-    def _testStreamProcessingFacts(self, filename):
-        self.activity.ServiceData["ActivityID"] = filename
-        self.activity.StartTime = self.activity.StartTime.replace(tzinfo=pytz.utc)
-        streamdata = FileLoader.getActivity(filename, None).json()
-        expected_waypoints = self.convertStreamsToWaypointsList(streamdata, self.activity.StartTime)
-        StravaService().DownloadActivity(self.svcRecord, self.activity, connection=FileLoader)
-        actual_waypoints = self.activity.Laps[0].Waypoints
-        self.assertWaypointsListsEqual(actual_waypoints, expected_waypoints)
-
-    def convertStreamsToWaypointsList(self, streamdata, startTime):
-        ridedata = {}
-        for stream in streamdata:
-            ridedata[stream["type"]] = stream["data"]
-
-        waypoints = []
-
-        hasHR = "heartrate" in ridedata and len(ridedata["heartrate"]) > 0
-        hasCadence = "cadence" in ridedata and len(ridedata["cadence"]) > 0
-        hasTemp = "temp" in ridedata and len(ridedata["temp"]) > 0
-        hasPower = ("watts" in ridedata and len(ridedata["watts"]) > 0)
-        hasAltitude = "altitude" in ridedata and len(ridedata["altitude"]) > 0
-        hasDistance = "distance" in ridedata and len(ridedata["distance"]) > 0
-        hasVelocity = "velocity_smooth" in ridedata and len(ridedata["velocity_smooth"]) > 0
-
-        inPause = False
-
-        waypointCt = len(ridedata["time"])
-        for idx in range(0, waypointCt - 1):
-
-            waypoint = Waypoint(startTime + timedelta(0, ridedata["time"][idx]))
-            if "latlng" in ridedata:
-                latlng = ridedata["latlng"][idx]
-                waypoint.Location = Location(latlng[0], latlng[1], None)
-                if waypoint.Location.Longitude == 0 and waypoint.Location.Latitude == 0:
-                    waypoint.Location.Longitude = None
-                    waypoint.Location.Latitude = None
-
-            if hasAltitude:
-                if not waypoint.Location:
-                    waypoint.Location = Location(None, None, None)
-                waypoint.Location.Altitude = float(ridedata["altitude"][idx])
-
-            # When pausing, Strava sends this format:
-            # idx = 100 ; time = 1000; moving = true
-            # idx = 101 ; time = 1001; moving = true  => convert to Pause
-            # idx = 102 ; time = 2001; moving = false => convert to Resume: (2001-1001) seconds pause
-            # idx = 103 ; time = 2002; moving = true
-
-            if idx == 0:
-                waypoint.Type = WaypointType.Start
-            elif idx == waypointCt - 2:
-                waypoint.Type = WaypointType.End
-            elif idx < waypointCt - 2 and ridedata["moving"][idx+1] and inPause:
-                waypoint.Type = WaypointType.Resume
-                inPause = False
-            elif idx < waypointCt - 2 and not ridedata["moving"][idx+1] and not inPause:
-                waypoint.Type = WaypointType.Pause
-                inPause = True
-
-            if hasHR:
-                waypoint.HR = ridedata["heartrate"][idx]
-            if hasCadence:
-                waypoint.Cadence = ridedata["cadence"][idx]
-            if hasTemp:
-                waypoint.Temp = ridedata["temp"][idx]
-            if hasPower:
-                waypoint.Power = ridedata["watts"][idx]
-            if hasVelocity:
-                waypoint.Speed = ridedata["velocity_smooth"][idx]
-            if hasDistance:
-                waypoint.Distance = ridedata["distance"][idx]
-            waypoints.append(waypoint)
-
-        return waypoints
